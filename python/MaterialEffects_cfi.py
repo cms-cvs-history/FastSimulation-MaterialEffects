5cd6f86df5fd09e92ee5f750480bad46919a2f12
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2009-04-08 08:27:54 +0000

    added material effects for muons in the calo

--- python/MaterialEffects_cfi.py
+++ python/MaterialEffects_cfi.py
@@ -138,9 +138,35 @@ MaterialEffectsBlock = cms.PSet(
    )
 )
 
+
+CommonMaterialEffectsForMuonsBlock = cms.PSet(
+    	# GEneral switches
+
+	# Enable photon pair conversion - Ignored 
+        PairProduction = cms.bool(True),
+	# Smallest photon energy allowed for conversion - Ignored
+        photonEnergy = cms.double(0.1),
+	# Enable muon Bremsstrahlung - currently not implemented 
+        Bremsstrahlung = cms.bool(False),
+	# Smallest bremstrahlung photon energy 
+        bremEnergy = cms.double(0.1),
+	# Smallest bremsstrahlung energy fraction (wrt to the electron energy)
+        bremEnergyFraction = cms.double(0.005),
+	# Enable dE/dx
+        EnergyLoss = cms.bool(False),
+	# Enable Multiple Scattering
+        MultipleScattering = cms.bool(True),
+	# Smallest pT for the Mutliple Scattering 
+        pTmin = cms.double(0.3),
+        # Nuclear Interaction - Ignored 
+        NuclearInteraction = cms.bool(False)
+    )
+
 MaterialEffectsForMuonsBlock = cms.PSet(
     MaterialEffectsForMuons = cms.PSet(
 
+        CommonMaterialEffectsForMuonsBlock,
+        
 	# Material Properties (Iron - this is for muons)
 	# A
         A = cms.double(55.8455),
@@ -150,27 +176,40 @@ MaterialEffectsForMuonsBlock = cms.PSet(
         Density = cms.double(7.87),
 	# One radiation length in cm
         RadiationLength = cms.double(1.76),
+    )
+)
 
-	# GEneral switches
-	# Enable photon pair conversion 
-        PairProduction = cms.bool(False),
-	# Smallest photon energy allowed for conversion
-        photonEnergy = cms.double(0.1),
-	# Enable electron Bremsstrahlung
-        Bremsstrahlung = cms.bool(False),
-	# Smallest bremstrahlung photon energy
-        bremEnergy = cms.double(0.1),
-	# Smallest bremsstrahlung energy fraction (wrt to the electron energy)
-        bremEnergyFraction = cms.double(0.005),
-	# Enable dE/dx
-        EnergyLoss = cms.bool(True),
-	# Enable Multiple Scattering
-        MultipleScattering = cms.bool(True),
-	# Smallest pT for the Mutliple Scattering 
-        pTmin = cms.double(0.3),
-	# Enable Nuclear Interactions
-        NuclearInteraction = cms.bool(False)
+MaterialEffectsForMuonsInECALBlock = cms.PSet(
+    MaterialEffectsForMuonsInECAL = cms.PSet(
 
+        CommonMaterialEffectsForMuonsBlock,
+
+	# Material Properties (PbW04 - this is for muons)
+	# A
+        A = cms.double(55.8455),
+	# Z
+        Z = cms.double(26.0),
+	# Density in g/cm3
+        Density = cms.double(8.280),
+	# One radiation length in cm
+        RadiationLength = cms.double(0.89),
+    )
+)
+
+MaterialEffectsForMuonsInHCALBlock = cms.PSet(
+    MaterialEffectsForMuonsInHCAL = cms.PSet(
+
+        CommonMaterialEffectsForMuonsBlock,
+
+	# Material Properties (BRASS - this is for muons)
+	# A
+        A = cms.double(64.0),
+	# Z
+        Z = cms.double(29.0),
+	# Density in g/cm3
+        Density = cms.double(8.5),
+	# One radiation length in cm
+        RadiationLength = cms.double(1.44),
     )
 )
 
